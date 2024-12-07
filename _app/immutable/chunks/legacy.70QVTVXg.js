757a870297f51973f909b63fd874f937cae71c55
Moonrise (Desktop) <aliuesch@gmail.com>
2024-12-06 21:40:26 -0700

    Updates

---
+++ _app/immutable/chunks/legacy.70QVTVXg.js
@@ -0,0 +1,1 @@
+import{e}from"./runtime.k-CIIIgP.js";e();
--- _app/immutable/chunks/paths.D04rE-Ev.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_i2h9a)==null?void 0:s.base)??"/test.github.io";var t;const e=((t=globalThis.__sveltekit_i2h9a)==null?void 0:t.assets)??a;export{e as a,a as b};
